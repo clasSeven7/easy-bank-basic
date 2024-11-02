b420547d2274320542ee766c9ddc41c3ff735505
saulojustiniano1 <saulojustiniano1@gmail.com>
2024-11-01 21:20:30 -0300

    Add initial HTML, CSS, and JavaScript files for Easy Bank website

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+let header = document.querySelector('.header');
+let navbar = document.querySelector('.header .flex .navbar');
+let menuBtn = document.querySelector('#menu-btn');
+
+menuBtn.onclick = () => {
+  navbar.classList.toggle('fa-xmark');
+  navbar.classList.toggle('active');
+};
+
+Window.onscroll = () => {
+  navbar.classList.remove('fa-xmark');
+  navbar.classList.remove('active');
+
+  if (window.scrollY > 0) {
+    header.classList.add('active');
+  } else {
+    header.classList.remove('active');
+  }
+};
